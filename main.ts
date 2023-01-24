c8f64bb407009041b22c8c355418cf839f20efc7
blueocean1234 <122539854+blueocean1234@users.noreply.github.com>
2023-01-24 08:35:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    basic.showString("Bye Adhya!")
+    basic.showString("\"Bye!!!!\"")
 })
 input.onGesture(Gesture.ScreenDown, function () {
     basic.showIcon(IconNames.Duck)
@@ -11,21 +11,9 @@ input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Rollerskate)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("Hello Adhya!")
+    basic.showString("Hello!")
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Heart)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    while (input.logoIsPressed()) {
-        basic.showLeds(`
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            `)
-    }
-    basic.clearScreen()
 })
 basic.showIcon(IconNames.Yes)
